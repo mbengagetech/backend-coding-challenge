f26bd5976d467e468acbdc3bf6707a6100ecdef8
M <mbengagetech@pobox.com>
2017-06-19 18:01:41 +0200

    #2 Add GET endpoint to fetch existing expenses

--- src/main/java/com/engage/expenses/repository/ExpensesRepository.java
+++ src/main/java/com/engage/expenses/repository/ExpensesRepository.java
@@ -10,7 +10,8 @@ import static com.engage.shared.Hibernate.runWithSession;
 public class ExpensesRepository {
 
     public List<Expense> findAll() {
-        return null;
+        return fetchWithSession(session -> (List<Expense>) session.createQuery("from Expense").getResultList());
+    }
 
     public void saveExpense(Expense expense) {
         runWithSession(session -> session.save(expense));
